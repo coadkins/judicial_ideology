49eaf33334bce6f8f31106e40b176b788625d8d1
Cory Adkins <adkins.cory@gmail.com>
2025-08-20 17:44:22 +0000

    sync metadata before tar_make()

---
+++ R/sync_targets_metadata.R
@@ -0,0 +1,13 @@
+targets::tar_option_set(
+  repository_meta = "aws",
+  resources = targets::tar_resources(
+    targets::tar_resources_aws(
+      bucket = Sys.getenv("S3_BUCKET"),
+      prefix = "main",
+      endpoint = Sys.getenv("S3_ENDPOINT"),
+      region = Sys.getenv("S3_REGION")
+    )
+  )
+)
+
+targets::tar_meta_sync()
