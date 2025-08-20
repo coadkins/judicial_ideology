36cfb8f0d0f6b4aadf7360529ea01e9b77231733
Cory Adkins <adkins.cory@gmail.com>
2025-08-20 00:24:24 +0000

    update S3 subdirectory for main branch

--- _targets.R
+++ _targets.R
@@ -20,7 +20,7 @@ tar_option_set(
   resources = tar_resources(
     tar_resources_aws(
       bucket = Sys.getenv("S3_BUCKET"),
-      prefix = "SBV",
+      prefix = "main",
       endpoint = Sys.getenv("S3_ENDPOINT"),
       region = Sys.getenv("S3_REGION")
     )
