28703695a649ec8a4698614f5351bf9246cf8c1f
Cory Adkins <adkins.cory@gmail.com>
2025-08-23 01:09:48 +0000

    fix S3 prefix in sync script

--- R/sync_targets_metadata.R
+++ R/sync_targets_metadata.R
@@ -3,7 +3,7 @@ targets::tar_option_set(
   resources = targets::tar_resources(
     targets::tar_resources_aws(
       bucket = Sys.getenv("S3_BUCKET"),
-      prefix = "sum-to-zero",
+      prefix = "ref-group",
       endpoint = Sys.getenv("S3_ENDPOINT"),
       region = Sys.getenv("S3_REGION")
     )
