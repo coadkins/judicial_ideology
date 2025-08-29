6579cd6e37deb2521401605a2f5d5d4c0588f1a3
Cory Adkins <adkins.cory@gmail.com>
2025-08-29 12:55:51 +0000

    don't use crew

--- _targets.R
+++ _targets.R
@@ -20,7 +20,6 @@ tar_option_set(
   repository = "aws",
   repository_meta = "aws",
   format = "qs",
-  controller = crew::crew_controller_local(workers = 2),
   resources = tar_resources(
     tar_resources_aws(
       bucket = Sys.getenv("S3_BUCKET"),
